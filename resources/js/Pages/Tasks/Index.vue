c4dc5172127dda341055e143c801baaf27dc51e5
Rizwan Azam <rizwanazam607@gmail.com>
2024-07-18 23:14:12 +0500

    initial commit

---
+++ resources/js/Pages/Tasks/Index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import TableTop from '@/Components/TableTop.vue';
+import Table from '@/Components/Table.vue';
+</script>
+
+<template>
+    <Head title="Tasks Management" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Tasks Management</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white p-8 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <TableTop />
+
+                    <Table />
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
